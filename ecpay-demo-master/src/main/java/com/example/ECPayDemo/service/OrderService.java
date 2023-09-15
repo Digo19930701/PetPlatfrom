b768d40f1cf8cb8fbae0de8e27142b7bf61f7cfa
WS26\User <a903480@gmail.com>
2023-09-15 13:45:28 +0800

    '0915'

--- ecpay-demo-master/src/main/java/com/example/ECPayDemo/service/OrderService.java
+++ ecpay-demo-master/src/main/java/com/example/ECPayDemo/service/OrderService.java
@@ -1,5 +1,8 @@
 package com.example.ECPayDemo.service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -11,15 +14,16 @@ import ecpay.payment.integration.domain.AioCheckOutALL;
 public class OrderService {
 
 	public String ecpayCheckout() {
-		
 		String uuId = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 20);
 		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String date = dateFormat.format(new Date());
+        
 		AllInOne all = new AllInOne("");
-		
 		AioCheckOutALL obj = new AioCheckOutALL();
 		obj.setMerchantTradeNo(uuId);
-		obj.setMerchantTradeDate("2017/01/01 08:05:23");
-		obj.setTotalAmount("20");
+		obj.setMerchantTradeDate(date);
+		obj.setTotalAmount("1314");
 		obj.setTradeDesc("test Description");
 		obj.setItemName("TestItem");
 		obj.setReturnURL("http://211.23.128.214:5000");
