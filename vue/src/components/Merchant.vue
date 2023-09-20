6cc3dc9f020d3e4cb2e6cd0174ab0d0f7605fe58
Cissto <sa8pfa.g5290@gmail.com>
2023-09-20 23:03:58 +0800

    09202303

--- vue/src/components/Merchant.vue
+++ vue/src/components/Merchant.vue
@@ -19,10 +19,10 @@
               <span class="sub-menu">商家設定</span>
             </template>
             <el-menu-item index="1-1">
-              <router-link to="/sellerSetting" style="font-size: 1.4rem">基本資料</router-link>
+              <router-link to="/sellerSetting" class="inner">基本資料</router-link>
             </el-menu-item>
             <el-menu-item index="1-2">
-              <router-link to="/notifySetting">通知設定</router-link>
+              <router-link to="/notifySetting" class="inner">通知設定</router-link>
             </el-menu-item>
           </el-sub-menu>
 
@@ -31,62 +31,62 @@
               <el-icon>
                 <document />
               </el-icon>
-              <span>服務管理</span>
+              <span class="sub-menu">服務管理</span>
             </template>
             <el-menu-item index="2-1">
-              <router-link to="/servicesExist">現有服務</router-link>
+              <router-link to="/servicesExist" class="inner">現有服務</router-link>
             </el-menu-item>
             <el-menu-item index="2-2">
-              <router-link to="/serviceEdit">新增/編輯服務</router-link>
+              <router-link to="/serviceEdit" class="inner">新增/編輯服務</router-link>
             </el-menu-item>
           </el-sub-menu>
 
           <el-sub-menu index="3">
             <template #title>
               <el-icon><DocumentCopy /></el-icon>
-              <span>訂單管理</span>
+              <span class="sub-menu">訂單管理</span>
             </template>
             <el-menu-item index="3-1">
-              <router-link to="/sellerOrder1">待辦訂單</router-link>
+              <router-link to="/sellerOrder1" class="inner">待辦訂單</router-link>
             </el-menu-item>
             <el-menu-item index="3-2">
-              <router-link to="/sellerOrder2">取消訂單</router-link>
+              <router-link to="/sellerOrder2" class="inner">取消訂單</router-link>
             </el-menu-item>
             <el-menu-item index="3-3">
-              <router-link to="/sellerOrder3">過去訂單</router-link>
+              <router-link to="/sellerOrder3" class="inner">過去訂單</router-link>
             </el-menu-item>
           </el-sub-menu>
 
           <el-sub-menu index="4">
             <template #title>
               <el-icon><Histogram /></el-icon>
-              <span>數據中心</span>
+              <span class="sub-menu">數據中心</span>
             </template>
             <el-menu-item index="4-1">
-              <router-link to="/sellerChart">數據管理</router-link>
+              <router-link to="/sellerChart" class="inner">數據管理</router-link>
             </el-menu-item>
             <el-menu-item index="4-1">
-              <router-link to="/sellerRate">評價管理</router-link>
+              <router-link to="/sellerRate" class="inner">評價管理</router-link>
             </el-menu-item>
           </el-sub-menu>
 
           <el-sub-menu index="5">
             <template #title>
               <el-icon><CreditCard /></el-icon>
-              <span>帳務管理</span>
+              <span class="sub-menu">帳務管理</span>
             </template>
             <el-menu-item index="5-1">
-              <router-link to="/bankAccount">銀行帳號</router-link>
+              <router-link to="/bankAccount" class="inner">銀行帳號</router-link>
             </el-menu-item>
             <el-menu-item index="5-1">
-              <router-link to="/accountingPage">我的進帳</router-link>
+              <router-link to="/accountingPage" class="inner">我的進帳</router-link>
             </el-menu-item>
           </el-sub-menu>
 
           <el-menu-item index="6">
             <router-link to="/notifyPage">
               <el-icon><Bell /></el-icon>
-              <span>通知中心</span>
+              <span class="sub-menu">通知中心</span>
             </router-link>
           </el-menu-item>
         </el-menu>
@@ -175,11 +175,19 @@ const handleClose = (key: string, keyPath: string[]) => {
   font-size: 1.5rem;
   font-weight: 600;
 }
+.inner {
+  font-size: 1.4rem;
+}
 .el-input {
   width: 300px;
 }
 
 .dialog-footer button:first-child {
   margin-right: 10px;
+}
+:root {
+  .el-menu {
+    border-right: transparent;
+  }
 }
 </style>
