6815f8bcc900caed67cc8c1c64a1a992d0d2a116
Tsailin_Yuan <swchloetl@gmail.com>
2023-09-26 02:40:51 +0800

    testing vue3+springBoot

---
+++ ecpay-demo-master/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bean.dataBean;
+
+@RestController
+public class MyController {
+
+	private dataBean studentRecords;
+
+	@RequestMapping("/test")
+	public String test() {
+		System.out.println("test!");
+		return "test";
+	}
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("eventName", "FIFA 2018");
+		return "index";
+	}
+
+	@GetMapping("/student/allstudent")
+	public dataBean getAllStudents() {
+		studentRecords = new dataBean();
+		studentRecords.setName("Ray");
+		studentRecords.setAge(30);
+
+		return studentRecords;
+	}
+}
