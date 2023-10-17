fc0f10c54b707e06a43db1955a44a81f1423992f
ChloeTsaiLin <swchloetl@gmail.com>
2023-10-17 15:02:42 +0800

    delete port of ref code.

--- dmotSdjpa/src/main/java/com/ispan/eeit/_00_init/dao/BaseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ispan.eeit._00_init.dao;
-
-public interface BaseRepository {
-	void rebuildTableAndIndex();
-	void loadInitData();
-}
--- dmotSdjpa/src/main/java/com/ispan/eeit/_00_init/service/BaseService.java
+++
@@ -1,5 +0,0 @@
-package com.ispan.eeit._00_init.service;
- 
-public interface BaseService {
-	public void initData();
-}
